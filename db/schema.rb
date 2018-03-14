48738e02938d2bffa8153ef089979a039eb717e6
John Treankler <johntreankler@gmail.com>
2018-03-14 16:53:12 -0600

    rails generate model... word:string:uniq

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314165807) do
+ActiveRecord::Schema.define(version: 20180314224801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180314165807) do
     t.string "word"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["word"], name: "index_buzzwords_on_word", unique: true
   end
 
 end
