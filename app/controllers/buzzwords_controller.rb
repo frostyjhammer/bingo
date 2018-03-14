5e780bff5b130b130cf51c74d7f78d6dcc389fc2
John Treankler <johntreankler@gmail.com>
2018-03-13 18:11:47 -0600

    buzzwords_controller.rb: pg connection

--- app/controllers/buzzwords_controller.rb
+++ app/controllers/buzzwords_controller.rb
@@ -2,14 +2,26 @@ class BuzzwordsController < ApplicationController
   def new
   end
   def create
+# 'render plain' below from http://guides.rubyonrails.org/getting_started.html
     render plain: params[:buzzword].inspect
+#   <ActionController::Parameters {"category"=>"Default", "buzzwords"=>"this\r\nthat\r\the other"} permitted: false>
 
     p = params[:buzzword]
     c = p['category']
     b = p['buzzwords'].split(/[\r\n]+/)
 
-    puts 'Category: ' + c
-    puts 'Buzzwords: ' + b.join(',')
-#   <ActionController::Parameters {"category"=>"Default", "buzzwords"=>"this\r\nthat\r\nother"} permitted: false>
+    puts; puts 'Category: ' + c; puts 'Buzzwords: ' + b.join(','); puts
+
+
+require 'pg'
+  begin
+    con = PG.connect :dbname => 'bingo_test', :user => 'rails_dev'
+    puts 'Server Version: ' + con.server_version.to_s
+  rescue PG::Error => e
+    puts e.message
+  ensure
+    con.close if con
   end
+end
+
 end
