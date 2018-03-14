935367a6536a6b648cd1da65e8cae9f2f4742081
John Treankler <johntreankler@gmail.com>
2018-03-14 16:36:18 -0600

    Fix quotes in con.exec INSERT

--- app/controllers/buzzwords_controller.rb
+++ app/controllers/buzzwords_controller.rb
@@ -1,4 +1,5 @@
 class BuzzwordsController < ApplicationController
+  require 'pg'
   def new
   end
   def create
@@ -7,37 +8,35 @@ class BuzzwordsController < ApplicationController
 #   <ActionController::Parameters {"category"=>"Default", "buzzwords"=>"this\r\nthat\r\the other"} permitted: false>
 
     p = params[:buzzword]
-    c = p['category']
-    b = p['buzzwords'].split(/[\r\n]+/)
+    category = p['category']
+    words = p['buzzwords'].split(/[\r\n]+/)
 
-    puts; puts 'Category: ' + c; puts 'Buzzwords: ' + b.join(','); puts
-
-
-require 'pg'
-  begin
-    con = PG.connect :dbname => 'postgres', :user => 'rails_dev', :password => 'Rails-Dev-4'
-    puts 'Server Version: ' + con.server_version.to_s.split('0').join('.')
-
-    user = con.user
-    db_name = con.db
-    pswd = con.pass
-
-    puts "User: #{user}"
-    puts "Database name: #{db_name}"
-#   puts "Password: #{pswd}"
+    puts
+    puts 'Category: ' + category
+    puts 'Buzzwords: ' + words.join(',')
     puts
 
-    puts "Let's try to write to the database, then!"
-##  con.exec "INSERT INTO buzzwords (category, word) VALUES ('Default','Postgresql Rules!')"
-##  con.exec "INSERT INTO buzzwords (category, word, created_at, updated_at) VALUES ('Default', 'Postgresql Rules!', Time.now, Time.now)"
-    con.exec "INSERT INTO buzzwords (category, word, created_at, updated_at) VALUES ('Default', 'Postgresql Rules!', NOW(), NOW())"
-    puts
+    begin
+      con = PG.connect :dbname => 'postgres', :user => 'rails_dev', :password => 'Rails-Dev-4'
+      puts 'Server Version: ' + con.server_version.to_s.split('0').join('.')
 
-  rescue PG::Error => e
-    puts e.message
-  ensure
-    con.close if con
+      user = con.user
+      db_name = con.db
+      pswd = con.pass
+#     puts "User: #{user}"
+#     puts "Database name: #{db_name}"
+#     puts "Password: #{pswd}"
+
+      words.each {|w|
+        puts "Let's update this buzzword, " + w
+        con.exec "INSERT INTO buzzwords (category, word, created_at, updated_at) VALUES ('" + category + "','" + w + "', NOW(), NOW())"
+      }
+
+    rescue PG::Error => e
+      puts e.message
+    ensure
+      con.close if con
+    end
   end
-end
 
 end
