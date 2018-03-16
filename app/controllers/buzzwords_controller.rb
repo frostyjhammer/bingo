6aa277b5600ecf05fe29e2c24680fbee13b0f1f5
John Treankler <johntreankler@gmail.com>
2018-03-16 10:10:06 -0600

    Add count of added words

--- app/controllers/buzzwords_controller.rb
+++ app/controllers/buzzwords_controller.rb
@@ -6,17 +6,20 @@ class BuzzwordsController < ApplicationController
     p = params[:buzzword]
     category = p['category']
     words = p['buzzwords'].split(/[\r\n]+/)
+    nwords = 0
     words.each {|w|
       puts 'Category: ' + category + '. Buzzword: ' + w + '.'
+      nwords += 1
       begin
         con = PG.connect :dbname => 'postgres', :user => 'rails_dev', :password => 'Rails-Dev-4'
-##      con.exec "INSERT INTO buzzwords (category, word, created_at, updated_at) VALUES ('" + category + "','" + w + "', NOW(), NOW())"
         con.exec "INSERT INTO buzzwords (category, word, created_at, updated_at) VALUES ('#{category}', '#{w}', NOW(), NOW())"
       rescue PG::Error => e
         puts e.message
+        nwords -= 1
       ensure
         con.close if con
       end
     }
+    puts 'Added: ' + nwords.to_s
   end
 end
