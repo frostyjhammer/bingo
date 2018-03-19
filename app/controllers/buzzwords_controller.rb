b0ccd36da6b338928c8ae211d9628f8f99a1e624
John Treankler <johntreankler@gmail.com>
2018-03-19 11:40:37 -0600

    No bangsign on strip

--- app/controllers/buzzwords_controller.rb
+++ app/controllers/buzzwords_controller.rb
@@ -12,24 +12,26 @@ class BuzzwordsController < ApplicationController
     words = p['buzzwords'].split(/[\r\n]+/)
     nwords = 0
     words.each {|word|
-      word = word.strip!
-      begin
-        con = PG.connect :dbname => 'postgres', :user => 'rails_dev', :password => 'Rails-Dev-4'
-        sql = "SELECT word FROM buzzwords WHERE category = '#{category}' AND word = '#{word}'"
-        res = con.exec(sql)
-        puts 'Word "' + res.getvalue(0,0) + '" already in Category "' + category + '"'
-      rescue
-
+      word = word.strip
+      if (word)
         begin
-          con.exec "INSERT INTO buzzwords (category, word, created_at, updated_at) VALUES ('#{category}', '#{word}', NOW(), NOW())"
-          nwords += 1
-          puts 'Word "' + word + '" added to Category "' + category + '"'
-        rescue PG::Error => e
-          puts e.message
-        end
+          con = PG.connect :dbname => 'postgres', :user => 'rails_dev', :password => 'Rails-Dev-4'
+          sql = "SELECT word FROM buzzwords WHERE category = '#{category}' AND word = '#{word}'"
+          res = con.exec(sql)
+          puts 'Word "' + res.getvalue(0,0) + '" already in Category "' + category + '"'
+        rescue
 
-      ensure
-        con.close if con
+          begin
+            con.exec "INSERT INTO buzzwords (category, word, created_at, updated_at) VALUES ('#{category}', '#{word}', NOW(), NOW())"
+            nwords += 1
+            puts 'Word "' + word + '" added to Category "' + category + '"'
+          rescue PG::Error => e
+            puts e.message
+          end
+
+        ensure
+          con.close if con
+        end
       end
     }
     notice = 'Added ' + number_with_delimiter(nwords.to_s) + ' buzzwords to Category: ' + category + '.'
