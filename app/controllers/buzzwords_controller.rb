c12b028a1a33e6699407f4c7d5e8f2478814b591
John Treankler <johntreankler@gmail.com>
2018-03-19 11:06:45 -0600

    First read first, write if not found

--- app/controllers/buzzwords_controller.rb
+++ app/controllers/buzzwords_controller.rb
@@ -12,14 +12,25 @@ class BuzzwordsController < ApplicationController
     words = p['buzzwords'].split(/[\r\n]+/)
     nwords = 0
     words.each {|word|
-      puts 'Category: ' + category + ' Buzzword: ' + word
-      nwords += 1
+
       begin
         con = PG.connect :dbname => 'postgres', :user => 'rails_dev', :password => 'Rails-Dev-4'
-        con.exec "INSERT INTO buzzwords (category, word, created_at, updated_at) VALUES ('#{category}', '#{word}', NOW(), NOW())"
-      rescue PG::Error => e
-        puts e.message
-        nwords -= 1
+        sql = "SELECT word FROM buzzwords WHERE category = '#{category}' AND word = '#{word}'"
+        res = con.exec(sql)
+        puts 'Word ' + res.getvalue(0,0) + ' already in Category ' + category
+      rescue
+
+        begin
+##        con = PG.connect :dbname => 'postgres', :user => 'rails_dev', :password => 'Rails-Dev-4'
+          con.exec "INSERT INTO buzzwords (category, word, created_at, updated_at) VALUES ('#{category}', '#{word}', NOW(), NOW())"
+          nwords += 1
+          puts 'Word ' + word + ' added to Category ' + category
+        rescue PG::Error => e
+          puts e.message
+##      ensure
+##        con.close if con
+        end
+
       ensure
         con.close if con
       end
