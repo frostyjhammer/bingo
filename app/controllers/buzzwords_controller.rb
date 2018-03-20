bd186958f2067f037889c8f83b6341459fdcbf68
John Treankler <johntreankler@gmail.com>
2018-03-20 16:36:45 -0600

    bump updated_at when dupe word is found

--- app/controllers/buzzwords_controller.rb
+++ app/controllers/buzzwords_controller.rb
@@ -10,7 +10,7 @@ class BuzzwordsController < ApplicationController
     p = params[:buzzword]
     category = p['category']
     words = p['buzzwords'].split(/[\r\n]+/)
-    nwords = 0
+    nadded = 0; nupdated = 0
     words.each {|word|
       word = word.strip
       if (word)
@@ -19,11 +19,19 @@ class BuzzwordsController < ApplicationController
           sql = "SELECT word FROM buzzwords WHERE category = '#{category}' AND word = '#{word}'"
           res = con.exec(sql)
           puts 'Word "' + res.getvalue(0,0) + '" already in Category "' + category + '"'
+
+          begin
+            con.exec "UPDATE buzzwords SET updated_at = NOW() WHERE category = '#{category}' AND word = '#{word}'"
+            nupdated += 1
+          rescue PG::Error => e
+            puts e.message
+          end
+
         rescue
 
           begin
             con.exec "INSERT INTO buzzwords (category, word, created_at, updated_at) VALUES ('#{category}', '#{word}', NOW(), NOW())"
-            nwords += 1
+            nadded += 1
             puts 'Word "' + word + '" added to Category "' + category + '"'
           rescue PG::Error => e
             puts e.message
@@ -34,8 +42,8 @@ class BuzzwordsController < ApplicationController
         end
       end
     }
-    notice = 'Added ' + number_with_delimiter(nwords.to_s) + ' buzzwords to Category: ' + category + '.'
-    puts notice
+    puts 'Added ' + number_with_delimiter(nadded.to_s) + ' buzzwords to Category: ' + category + '.' if nadded > 0
+    puts 'Updated ' + number_with_delimiter(nupdated.to_s) + ' buzzwords to Category: ' + category + '.' if nupdated > 0
     redirect_to(:new_buzzword, notice: notice)
   end
 
